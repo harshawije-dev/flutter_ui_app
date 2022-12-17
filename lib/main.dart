1977d302272f34b16f052d9c74e09b10ee2a620b
harsha_Wije <harshawije99@gmail.com>
2022-12-17 12:10:49 +0530

    developing the responsive design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_app/layout.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
           }),
           primaryColor: Colors.blue[400]),
       title: "Dashboard",
-      home: Container(),
+      home: SiteLayout(),
     );
   }
 }
